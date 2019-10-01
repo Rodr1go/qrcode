1cd59a8eb5f0a72f23d628b2f5f760fd209e5fdb
Rodr1go <rodrigosc.pi@gmail.com>
2019-10-01 18:08:08 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# qrcode
+Gerador de QRCode com NodeJS e Express
+
+Para testar o projeto:
+---------------------- 
+
+### Clone o repositório
+`$ git clone https://github.com/Rodr1go/qrcode.git`
+
+### Entre no diretório do projeto via terminal
+`$ cd qrcode`
+
+### Instale as dependências
+`$ npm install`
+
+### Inicie a aplicação
+`$ npm start`
---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+
+const qr = require('qr-image')
+
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('home'))
+
+app.get('/qrcode', (req, res) => {
+  
+  const url = 'https://github.com/Rodr1go'
+  const code = qr.image(url, { type: 'svg' })
+
+  res.type('svg')
+  code.pipe(res)
+})
+
+app.listen(3000, () => {
+  console.log('server running...')
+})
