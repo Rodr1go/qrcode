2a2daba521740d6b0b27f8f6536c65b5ac85d3e6
Rodr1go <rodrigosc.pi@gmail.com>
2019-10-01 18:33:24 -0300

    organize files in src directory

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ const app = express()
 const qr = require('qr-image')
 
 app.set('view engine', 'ejs')
+app.set('views', './src/views');
+
 app.get('/', (req, res) => res.render('home'))
 
-app.get('/qrcode', (req, res) => {
-  
+app.get('/qrcode', (req, res) => {  
   const url = 'https://github.com/Rodr1go'
   const code = qr.image(url, { type: 'svg' })
 
   res.type('svg')
+  
   code.pipe(res)
 })
 
